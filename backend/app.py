c714b8ed766cafa47b148e8663758f1ec853a318
KauaN-png <KauaN-png@users.noreply.github.com>
2026-06-16 20:00:28 -0300

    fix: altera porta para 5000 e corrige Dockerfile multi-stage

--- backend/app.py
+++ backend/app.py
@@ -146,5 +146,5 @@ def compile_code():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
